1d4c5602bf2e914d6a17439f64f05e00ab07ddfd
kevin matthew T <kmturbobro02@gmail.com>
2024-10-30 20:08:32 +0700

    css change

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -10,9 +10,9 @@ function Sidebar() {
         <div className='sidebar-container'>
           
           <div className='pages-list'>
-            <h2 onClick={()=>navigate("/dashboard")}>Dashboard</h2>
-            <h2 onClick={()=>navigate("/investments")}>Investments</h2>
-            <h2 onClick={()=>navigate("/investments/form")}>Add New</h2>
+            <h2 onClick={()=>navigate("/dashboard")} className='page-item'>Dashboard</h2>
+            <h2 onClick={()=>navigate("/investments")} className='page-item'>Investments</h2>
+            <h2 onClick={()=>navigate("/investments/form")} className='page-item'>Add New</h2>
           </div>
         </div>
       </>
